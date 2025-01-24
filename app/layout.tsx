61aeab8c97b40f09a41eac1ccdf33372b45b5eb9
sidendev <96108993+sidendev@users.noreply.github.com>
2025-01-24 23:00:06 +0000

    Remove unused fonts from layout file

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { EnvVarWarning } from '@/components/env-var-warning';
 import HeaderAuth from '@/components/header-auth';
 import { ThemeSwitcher } from '@/components/theme-switcher';
 import { hasEnvVars } from '@/utils/supabase/check-env-vars';
-import { Geist, Inter } from 'next/font/google';
+import { Inter } from 'next/font/google';
 import { ThemeProvider } from 'next-themes';
 import Link from 'next/link';
 import './globals.css';
@@ -17,11 +17,6 @@ export const metadata = {
     title: 'Next.js and Supabase Starter Kit',
     description: 'The fastest way to build apps with Next.js and Supabase',
 };
-
-const geistSans = Geist({
-    display: 'swap',
-    subsets: ['latin'],
-});
 
 const inter = Inter({
     subsets: ['latin'],
